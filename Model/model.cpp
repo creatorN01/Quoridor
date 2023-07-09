1d064a8521d16a57f70ccaa7ce4551141e08ac17
菜鸭 <3525472411@qq.com>
2023-07-09 15:39:30 +0800

    model.cpp
    add some functions and interfaces

--- Model/model.cpp
+++ Model/model.cpp
@@ -4,3 +4,31 @@ Model::Model()
 {
 
 }
+
+Map Model::GetMap() /*only read data*/
+{
+    return this->WholeMap;
+}
+
+Map& Model::GetMapReference()/*when need to change map*/
+{
+    return this->WholeMap;
+}
+
+void Model::test()/*demo for usage of some functions*/
+{
+    Map tmp = GetMapReference();
+
+    tmp.Remove(PlayerId::FIRST,std::pair<int,int>(2,3),std::pair<int,int>(3,3));
+
+    tmp.Add(PlayerId::SECOND,std::pair<int,int>(4,4),std::pair<int,int>(5,4));
+
+    Map t = GetMap();
+
+    bool accessible = t.Accessible(std::pair<int,int>(3,4),std::pair<int,int>(4,4));
+    if(accessible){}
+
+    bool hasSolution = GetMap().JudgeSolution(PlayerId::FIRST,3,3);
+    if(hasSolution){}
+
+}
